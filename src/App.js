517360a0efe1cc8a91d30373651e525aa73b8a5f
3기-1반 애범 <gogot1103@naver.com>
2022-01-17 21:56:39 +0900

    recoil todoList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 import { RecoilRoot } from "recoil";
+import TodoList from "./todo/TodoList";
 
 function App() {
+  // recoil을 사용하려면 최상단 컴포넌트에서 RecoilRoot로 감싸줘야 한다.
   return (
     <RecoilRoot>
-      <h1>Todo</h1>
+      <TodoList />
     </RecoilRoot>
   );
 }
