39ec09070b5556a70d3e1718203d486a76a67834
3기-1반 애범 <gogot1103@naver.com>
2022-01-17 17:28:06 +0900

    recoil 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { RecoilRoot } from "recoil";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <h1>Todo</h1>
+    </RecoilRoot>
+  );
+}
+
+export default App;
